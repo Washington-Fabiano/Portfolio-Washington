71ec5f70eb30f01c3d6a9842e0ea6fb4883ef0a1
Washington-Fabiano <washington_vieira@outlook.com>
2022-04-01 18:56:04 -0300

    update

---
+++ assets/js/componentes/carousel.js
@@ -0,0 +1,16 @@
+const btnLeft = document.querySelector('.carousel .button-arrow.left');
+const btnRight = document.querySelector('.carousel .button-arrow.right');
+const elements = document.querySelector('.elements');
+let pixels = 100;
+
+btnRight.addEventListener('click', function() {
+    pixels = pixels + 100;
+    elements.style = `transform: translateX(${pixels}px)`;
+    
+})
+
+btnLeft.addEventListener('click', function() {
+    pixels = pixels - 100;
+    elements.style = `transform: translateX(${pixels}px)`;
+    
+})
